e6f1d8ecc0e1fd34f3caa069966155bf4fadf788
rtolstovn <rtolstovn@gmail.com>
2023-10-10 17:59:33 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+import {Home} from './Pages/Home'
+import { About } from './Pages/About';
+import { Contact } from './Pages/Contact';
+import { Category } from './Pages/Category';
+import { NotFound } from './Pages/NotFound';
+import { Recipe } from './Pages/Recipe';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router basename= '/react-food'>
+                <Header />
+                <main className='container content'>
+                
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/about' element={<About />} />
+                        <Route path='/contacts' element={<Contact />} />
+                        <Route path='/category/:name' element={<Category />} />
+                        <Route path='/meal/:id' element={<Recipe />} />
+                        <Route element={<NotFound />} />
+                    </Routes>
+                    
+                </main>
+                <Footer />
+            </Router>
+        </>
+    );
 }
 
 export default App;
